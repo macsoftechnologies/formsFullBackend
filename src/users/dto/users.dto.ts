fdb3757960a686b5b1413f5b5072b139ee0afc77
Macsoftechnologies <macsoftechnologies@gmail.com>
2021-07-02 14:17:44 +0530

    changes  in users  forms

--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -1,50 +1,70 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
-export  class formStatus{
-    form:string
-    formStatus:string
-}
-export  class CompanyIdStatus{
-    CompanyId:string
-    companyIdStatus:string
-}
-export  class EmailStatus{
+// export  class formStatus{
+//     form:string
+//     formStatus:string
+// }
+// export  class CompanyIdStatus{
+//     CompanyId:string
+//     companyIdStatus:string
+// }
+// export  class Email{
+//     EmailId:string
+//     emailIdStatus:string
+// }
+// export  class LastName{
+//     lastName:string
+//     lastNameStatus:string
+// }
+// export  class FirstNameStatus{
+//     firstName:string
+//     firstNameStatus:string
+// }
+// export  class PhoneNumberStatus{
+//     phoneNumber:string
+//     phNumberStatus:string
+// }
+// export  class StateStatus{
+//     state:string
+//     stateStatus:string
+// }
+// export  class CountryStatus{
+//     country:string
+//     countryStatus:string
+// }
+
+export  class Email{
     EmailId:string
     emailIdStatus:string
 }
-export  class LastNameStatus{
-    lastName:string
+export class lName {
+    lname:string
+}
+export class LastName{
+    lastname:string
+    lName:lName[]
     lastNameStatus:string
 }
-export  class FirstNameStatus{
+export class fName{
+    fname:string
+}
+export  class FirstName{
     firstName:string
+    fName:fName[]
     firstNameStatus:string
-}
-export  class PhoneNumberStatus{
-    phoneNumber:string
-    phNumberStatus:string
-}
-export  class StateStatus{
-    state:string
-    stateStatus:string
-}
-export  class CountryStatus{
-    country:string
-    countryStatus:string
 }
 
 export class usersDto{
    
-    Forms: formStatus[]
-    Email :EmailStatus[]
-
-    CompanyId:CompanyIdStatus
-    FirstName:FirstNameStatus
-    LastName:LastNameStatus
-    PhoneNumber:PhoneNumberStatus
-    Country:CountryStatus
-    State:StateStatus
-    UserId:string
-    UserStatus:string
+  //formStatus: string
+  Email: string
+ // CompanyIdStatus: string
+  FirstName: string
+  LastName: string
+//   PhoneNumberStatus: string
+//   CountryStatus: string
+//   StateStatus: string
+  UserId:string
+  UserStatus:string
 }
 
 export class DeleteUserDto{
--- src/users/schema/users.schema.ts
+++ src/users/schema/users.schema.ts
@@ -1,68 +1,77 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from 'mongoose';
 import { v4 as uuid } from 'uuid';
-export  class formStatus{
-    form:string
-    @Prop({default:"active"})
-    formStatus:string
+@Schema({ timestamps: true })
+export class users extends Document{
+   
+    // @Prop()
+    // CompanyId: string
+    // @Prop()
+    // form: string
+    @Prop({ unique:true , default : uuid})
+    UserId: string
+    @Prop({ unique:true})
+    Email: Email[]
+    @Prop({default: "Active"})
+    UserStatus: string
+    @Prop()
+    FirstName: FirstName[]
+    @Prop()
+    LastName: LastName[]
+    // @Prop()
+    // PhoneNumber: string
+    // @Prop()    
+    // Country: string
+    // @Prop()
+    // State: string
 }
-export  class CompanyIdStatus{
-    CompanyId:string
-    @Prop({default:"pending"})
-    companyIdStatus:string
-}
-export  class EmailStatus{
+export const  usersSchema = SchemaFactory.createForClass(users);
+
+// export  class form{
+//     form:string
+//     @Prop({default:"active"})
+//     formStatus:string
+// }
+// export  class CompanyId{
+//     CompanyId:string
+//     @Prop({default:"pending"})
+//     companyIdStatus:string
+// }
+export  class Email{
     EmailId:string
     @Prop({default:"pending"})
     emailIdStatus:string
 }
-export  class LastNameStatus{
-    lastName:string
+export class lName {
+    lname:string
+}
+export class LastName{
+    lastname:string
+    LName:lName[]
     @Prop({default:"pending"})
     lastNameStatus:string
 }
-export  class FirstNameStatus{
+export class fName{
+    fname:string
+}
+export  class FirstName{
     firstName:string
+    fName:fName[]
     @Prop({default:"pending"})
     firstNameStatus:string
 }
-export  class PhoneNumberStatus{
-    phoneNumber:string
-    @Prop({default:"pending"})
-    phNumberStatus:string
-}
-export  class StateStatus{
-    state:string
-    @Prop({default:"pending"})
-    stateStatus:string
-}
-export  class CountryStatus{
-    country:string
-    @Prop({default:"pending"})
-    countryStatus:string
-}
-@Schema({ timestamps: true })
-export class users extends Document{
-   
-    @Prop()
-    CompanyId: CompanyIdStatus
-    @Prop()
-    Forms: formStatus
-    @Prop({ unique:true , default : uuid})
-    UserId: string
-    @Prop({required : true, unique:true})
-    Email: EmailStatus
-    @Prop({default: "Active"})
-    UserStatus: string
-    @Prop()
-    FirstName: FirstNameStatus
-    @Prop()
-    LastName: LastNameStatus
-    @Prop()
-    PhoneNumber: PhoneNumberStatus
-    @Prop()    
-    Country: CountryStatus
-    @Prop()
-    State: StateStatus
-}
-export const  usersSchema = SchemaFactory.createForClass(users);
+// export  class PhoneNumber{
+//     phoneNumber:string
+//     @Prop({default:"pending"})
+//     phNumberStatus:string
+// }
+// export  class State{
+//     state:string
+//     @Prop({default:"pending"})
+//     stateStatus:string
+// }
+// export  class Country{
+//     country:string
+//     @Prop({default:"pending"})
+//     countryStatus:string
+//}
