7bf20c5b4b729a2ad02957b9b299e874436755b5
Macsoftechnologies <macsoftechnologies@gmail.com>
2021-07-07 14:17:22 +0530

    form type changes

--- src/form-type/dto/formType.dto.ts
+++ src/form-type/dto/formType.dto.ts
@@ -1,11 +1,19 @@
 import { IsNotEmpty } from 'class-validator';
+export class addmore{
+    addmore:string
+}
+
+export class form_Type{
+    formType:string
+}
+
+export class endPoint{
+  endpoint:string
+}
 export class formTypeDto{
-   
-    
-    
     @IsNotEmpty()
-    Form_Type :string
+    Form_Type :form_Type[]
     @IsNotEmpty()
-    Endpoint:string
-    AddMore:string
+    Endpoint:endPoint[]
+    AddMore:addmore[]
 }
